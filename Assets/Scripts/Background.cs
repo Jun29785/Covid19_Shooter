c39117887544fe0cf9c97de273a2846bfa511759
Jun29785 <leejun9334@gmail.com>
2022-03-07 15:50:01 +0900

    Init
    파일 이동

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    public float ScrollSpeed = 10.0f;
+    private float ResetTiming = 5.0f;
+    private float Timing = 0.0f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        ResetBackground();
+    }
+
+    void ResetBackground()
+    {
+        Timing += Time.deltaTime;
+        if (ResetTiming < Timing)
+        {
+            // Reset
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerBullet.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBullet : MonoBehaviour
+{
+    private float BulletSpeed = 3.0f;
+    Rigidbody2D rb;
+    float Timer = 5.0f;
+    float CurrentTimer = 0.0f;
+    
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Move();
+        DestroyTimer(); 
+    }
+
+    void Move()
+    {
+        rb.AddForce(Vector2.up * BulletSpeed, ForceMode2D.Force);
+    }
+    
+    void DestroyTimer()
+    {
+        CurrentTimer += Time.deltaTime;
+        if (Timer < CurrentTimer)
+        {
+            Debug.Log("Destroy");
+            Destroy(this.gameObject);
+        }
+        Debug.Log("currentTimer : " + CurrentTimer);
+    }
+}
